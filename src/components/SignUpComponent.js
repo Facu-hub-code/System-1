d9c904a6f547d409912ef11721eafc6c2cfa901a
Facundo Lorenzo <faculoren7@gmail.com>
2024-02-05 17:57:36 -0300

    cambios esteticos

--- src/components/SignUpComponent.js
+++ src/components/SignUpComponent.js
@@ -1,45 +1,27 @@
-// SignUpPage.js
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import FormComponent from '../components/FormComponent';
+import React from 'react';
+import '../styles/SignUpPage.css';
+
 
 const SignUpPage = () => {
-  const [formData, setFormData] = useState({
-    company_name: '',
-    email: '',
-  });
-
-  const handleFormSubmit = (data) => {
-    setFormData(data);
+    return (
+      <div className="signup-page">
+        <div className="left-section">
+          {/* Background */}
+          <img className="background-image" src={require('../images/background.png')} alt="Background" />
+  
+          {/* Logo */}
+          <img className="marvel-logo" src={require('../images/MarvelLogo.png')} alt="Marvel Logo" />
+        </div>
+  
+        <div className="right-section">
+          <h1 className="account-signup">Account Sign-up</h1>
+          <p className="welcome-message">
+            Welcome! You can sign-up using email and a company name. Or using Google!
+          </p>
+        
+        </div>
+      </div>
+    );
   };
-
-  return (
-    <div className="signup-page">
-      <div className="left-section">
-        {/* Background */}
-        <img className="background-image" src={require('../images/background.png')} alt="Background" />
-
-        {/* Logo */}
-        <img className="marvel-logo" src={require('../images/MarvelLogo.png')} alt="Marvel Logo" />
-      </div>
-
-      <div className="right-section">
-        <h1 className="account-signup">Account Sign-up</h1>
-        <p className="welcome-message">
-          Welcome! You can sign-up using email and a company name. Or using Google!
-        </p>
-        <FormComponent onSubmit={handleFormSubmit} />
-        <Link
-          to={{
-            pathname: '/confirmation',
-            state: formData,
-          }}
-        >
-          
-        </Link>
-      </div>
-    </div>
-  );
-};
-
-export default SignUpPage;
+  
+  export default SignUpPage;
