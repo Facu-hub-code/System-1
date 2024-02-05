6d076d5ccfb634d73f9201c94975c126f5678b58
Facundo Lorenzo <faculoren7@gmail.com>
2024-02-05 16:50:05 -0300

    del google auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 
 import './App.css';
-import { GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from 'jwt-decode';
 import MyForm from './MyForm';
 function App() {
   console.log('Renderizando App');
   return (
     <div>
-      {/* Adds the oauth google button */}
-      <GoogleLogin
-        className="google-login-button"
-        onSuccess={credentialResponse => {
-          const credentialResponseDecoded = jwtDecode(
-            credentialResponse.credential
-          );
-          console.log(credentialResponseDecoded);
-        }}
-        onError={() => {
-          console.log('Login Failed');
-        }}
-      />
         <MyForm
           onSuccess={data => {
             console.log('Formulario enviado con éxito');
--- src/index.js
+++ src/index.js
@@ -4,24 +4,14 @@ import './index.css';
 import App from './App';
 import SignUpPage from './components/SignUpPage';
 import reportWebVitals from './reportWebVitals';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-      <>
         <App />
-        <SignUpPage />
-      </>
-    </GoogleOAuthProvider>
   </React.StrictMode>
 );
-
-
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
