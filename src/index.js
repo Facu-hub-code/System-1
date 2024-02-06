76e2a37338a4052a8e0e4f7071929b4c1433ccb3
facu <facu2@marveladvisors.com>
2024-02-06 11:02:59 -0300

    react-route works properly, needs to implement security

--- src/index.js
+++ src/index.js
@@ -3,19 +3,20 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route } from 'react-router-dom';
-
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import MyForm from './MyForm';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-      <BrowserRouter>
+  <BrowserRouter>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+      <>
         <App />
-    </BrowserRouter>,
-  </React.StrictMode>
+      </>
+    </GoogleOAuthProvider>
+  </BrowserRouter>
 );
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
