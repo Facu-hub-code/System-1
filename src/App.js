e95fe47569d582c93fcf340642f788bddaf2ba41
facu <facu2@marveladvisors.com>
2024-02-06 12:02:02 -0300

    fixed merge problems

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 
 import './App.css';
 import { jwtDecode } from 'jwt-decode';
-import GroupComponent from './components/GroupComponent'
-import WelcomeBanner from './components/WelcomeBanner';
+import { Routes, Route } from 'react-router-dom';
+import SignUpComponent from './components/SignUpComponent';
+import ConfirmationPage from './components/ConfirmationPage';
+
 function App() {
   console.log('Renderizando App');
   return (
     <div>
-      <div>
-        <WelcomeBanner></WelcomeBanner>
-      </div>
-      <div>
-        <GroupComponent></GroupComponent>
-      </div>
+      {/* Adds the oauth google button */}
+      <GoogleLogin
+        className="google-login-button"
+        onSuccess={credentialResponse => {
+          const credentialResponseDecoded = jwtDecode(
+            credentialResponse.credential
+          );
+          console.log(credentialResponseDecoded);
+        }}
+        onError={() => {
+          console.log('Login Failed');
+        }}
+      />
+      <Routes>
+        <Route path="/" element={<SignUpComponent />} />
+        <Route path="/confirmation" element={<ConfirmationPage />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+      
+  
     </div>
     );
 }
