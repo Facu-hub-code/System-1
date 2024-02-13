069c4ae6d327f479b655d2d9c7a4cd417f46cad2
facu <facu2@marveladvisors.com>
2024-02-13 09:27:44 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 
 import './App.css';
-import { jwtDecode } from 'jwt-decode';
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f0450db (fixed merge problems)
 import { Routes, Route } from 'react-router-dom';
 import SignUpComponent from './components/SignUpComponent';
 import ConfirmationPage from './components/ConfirmationPage';
@@ -20,7 +15,6 @@ function App() {
   console.log('Renderizando App');
   return (
     <div>
-<<<<<<< HEAD
       {/* Adds the oauth google button */}
       {/* <GoogleLogin
         className="google-login-button"
@@ -33,35 +27,12 @@ function App() {
         onError={() => {
           console.log('Login Failed');
         }}
-<<<<<<< HEAD
-<<<<<<< HEAD
       /> */}
-=======
-      />
->>>>>>> f0450db (fixed merge problems)
-=======
-      /> */}
->>>>>>> d6a2d8e (route /configuration  secured)
       <Routes>
         <Route path="/" element={<SignUpComponent />} />
         <Route path="/confirmation" element={<ConfirmationPage />} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
-      
-<<<<<<< HEAD
-
-
-=======
-      <div>
-        <WelcomeBanner></WelcomeBanner>
-      </div>
-      <div>
-        <GroupComponent></GroupComponent>
-      </div>
->>>>>>> ab87f3d (cambios esteticos)
-=======
-  
->>>>>>> f0450db (fixed merge problems)
     </div>
     );
 }
