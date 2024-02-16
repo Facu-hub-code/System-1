db602fa2ac6cb66c0b603105bd0b9feb9135df8f
facu <facu2@marveladvisors.com>
2024-02-16 11:38:23 -0300

    test

--- back-end/server.js
+++ back-end/server.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 app.use(bodyParser.json());
 
 // Configuración de CORS
-const allowedOrigins = ['http://localhost', 'http://127.0.0.1', 'http://vsq.marveladvisors.com'];
+const allowedOrigins = ['http://localhost:3002','http://localhost', 'http://127.0.0.1', 'http://vsq.marveladvisors.com'];
 const corsOptions = {
   origin: function (origin, callback) {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
